d9bdc04402fbfa6214ed74f7d025a8a9d77b5107
kalis <kalis30nov@yahoo.com>
2023-06-05 06:17:44 +0530

    up

--- data.tf
+++ data.tf
@@ -2,6 +2,6 @@ data "aws_ssm_parameter" "db_user" {
   name = "${var.env}.${var.name}.db_user"
 }
 
-data "aws_ssm_parameter" "db_pass" {
+data "aws_ssm_parameter" "db_password" {
   name = "${var.env}.${var.name}.db_password"    
 }
